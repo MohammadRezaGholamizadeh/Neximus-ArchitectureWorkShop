84bc9f1eb778a327ef9bbf9297d3e4ea1814bb84
webhouse\masyg <masy.golriz@gmail.com>
2025-05-20 17:33:42 +0330

    changes for dissolving conflicts

--- Neximus.WorkShop/src/Neximus.WorkShop.Infrastructures/Neximus.WorkShop.Persistance/Products/Products/ProductEntityMap.cs
+++ Neximus.WorkShop/src/Neximus.WorkShop.Infrastructures/Neximus.WorkShop.Persistance/Products/Products/ProductEntityMap.cs
@@ -1,43 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Neximus.WorkShop.Domain.Products.Products;
-<<<<<<< HEAD
-using Neximus.WorkShop.Persistance.Products.Categories;
-=======
->>>>>>> origin/main
 
 namespace Neximus.WorkShop.Persistance.Products.Products
 {
     public class ProductEntityMap : IEntityTypeConfiguration<Product>
     {
-<<<<<<< HEAD
-    //    public long Id { get; set; }
-    //    public string Name { get; set; }
-    //    public decimal Price { get; set; }
-    //    public int Stock { get; set; }
-    //    public long CategoryId { get; set; }
-    //    public ProductCategory ProductCategory { get; set; }
-    //    public int PaperCount { get; set; }
-    //    public string AuthorName { get; set; }
-    //    public DateTime PublishDate { get; set; }
-    //    public string ISBN { get; set; }
-    //    public decimal Discount { get; set; }
-
-        public void Configure(EntityTypeBuilder<Product> _)
-        {
-            _.HasKey(_ => _.Id);
-            _.Property(_ => _.Name);
-            _.Property(_ => _.Price);
-            _.Property(_ => _.Stock);
-            _.Property(_ => _.CategoryId);
-            _.HasOne(o => o.ProductCategory);
-            _.Property(_ => _.PaperCount);
-            _.Property(_ => _.AuthorName);
-            _.Property(_ => _.PublishDate);
-            _.Property(_ => _.ISBN);
-            _.Property(_ => _.Discount);
-            
-=======
         public void Configure(EntityTypeBuilder<Product> _)
         {
             _.ToTable("Product");
@@ -55,7 +23,6 @@ namespace Neximus.WorkShop.Persistance.Products.Products
             _.Property(_ => _.PublishDate).IsRequired();
             _.Property(_ => _.ISBN).IsRequired();
             _.Property(_ => _.Discount).IsRequired();
->>>>>>> origin/main
         }
     }
 }
