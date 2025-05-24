26c344f87925167c5da70d41e0585f3ee5c41668
Fatemeh <fatemeh.ganbari1993@gmail.com>
2025-05-24 21:32:36 +0330

    feat => add migration implimented

--- Neximus.WorkShop/src/Neximus.WorkShop.Infrastructures/Neximus.WorkShop.Migrations/MigrationRunner.cs
+++ Neximus.WorkShop/src/Neximus.WorkShop.Infrastructures/Neximus.WorkShop.Migrations/MigrationRunner.cs
@@ -3,100 +3,98 @@ using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Neximus.WorkShop.Migrations
+namespace Neximus.WorkShop.Migrations;
+// secuence of migration files is important
+public class MigrationRunner
 {
-    public class MigrationRunner
+    private readonly MigrationSettings _object;
+    private static MigrationSettings _connectionString;
+
+    public static void Main(string[] args)
     {
-        private readonly MigrationSettings _object;
-        private static MigrationSettings _connectionString;
-
-        public static void Main(string[] args)
-        {
-            _connectionString =
-                 MigrationSettings.Instance.GetConnectionString();
-            RunRootMigrations(args, _connectionString.ConnectionString);
-        }
-
-        public static void RunRootMigrations(string[]? args, string connectionString)
-        {
-            var options = connectionString;
-
-            CreateDatabaseSchema(connectionString);
-
-            var runner = CreateRunner(connectionString, _connectionString);
-            runner.MigrateUp();
-        }
-
-        public static void CreateDatabaseSchema(string connectionString)
-        {
-            var databaseName = GetDatabaseName(connectionString);
-            string masterConnectionString = ChangeDatabaseName(
-                connectionString, "master");
-            var commandScript =
-                $"if db_id(N'{databaseName}') is null create database" +
-                $" [{databaseName}]";
-
-            using var connection = new SqlConnection(masterConnectionString);
-            using var command = new SqlCommand(commandScript, connection);
-            connection.Open();
-            command.ExecuteNonQuery();
-            connection.Close();
-        }
-
-        private static string ChangeDatabaseName(
-            string connectionString, string databaseName)
-        {
-            var csb = new SqlConnectionStringBuilder(connectionString)
-            {
-                InitialCatalog = databaseName
-            };
-            return csb.ConnectionString;
-        }
-
-        private static string GetDatabaseName(string connectionString)
-        {
-            return new SqlConnectionStringBuilder(
-                connectionString).InitialCatalog;
-        }
-
-        private static FluentMigrator.Runner.IMigrationRunner CreateRunner(
-            string connectionString, MigrationSettings options)
-        {
-            var container = new ServiceCollection()
-                .AddFluentMigratorCore()
-                .ConfigureRunner(_ => _
-                    .AddSqlServer()
-                    .WithGlobalConnectionString(connectionString)
-                    .ScanIn(typeof(MigrationRunner).Assembly).For.All())
-                .AddSingleton<MigrationSettings>(options)
-                .AddSingleton<ScriptResourceManager>()
-                .AddLogging(_ => _.AddFluentMigratorConsole())
-                .BuildServiceProvider();
-            return container.GetRequiredService<FluentMigrator.Runner.IMigrationRunner>();
-        }
+        _connectionString = MigrationSettings.Instance.GetConnectionString();
+        RunRootMigrations(args, _connectionString.ConnectionString);
     }
 
-    // setting
-    public class MigrationSettings
+    public static void RunRootMigrations(string[]? args, string connectionString)
     {
-        public string ConnectionString { get; set; }
-        public static MigrationSettings Instance => new MigrationSettings();
+        var options = connectionString;
+
+        CreateDatabaseSchema(connectionString);
+
+        var runner = CreateRunner(connectionString, _connectionString);
+        runner.MigrateUp();
     }
 
-    public static class MigrationTools
+    public static void CreateDatabaseSchema(string connectionString)
     {
+        var databaseName = GetDatabaseName(connectionString);
+        string masterConnectionString = ChangeDatabaseName(
+            connectionString, "master");
+        var commandScript =
+            $"if db_id(N'{databaseName}') is null create database" +
+            $" [{databaseName}]";
 
-        public static MigrationSettings GetConnectionString(this MigrationSettings settings)
+        using var connection = new SqlConnection(masterConnectionString);
+        using var command = new SqlCommand(commandScript, connection);
+        connection.Open();
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    private static string ChangeDatabaseName(
+        string connectionString, string databaseName)
+    {
+        var csb = new SqlConnectionStringBuilder(connectionString)
         {
-            var configuration =
-                new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("MigrationSetting.json")
-                    .AddEnvironmentVariables()
-                    .Build();
+            InitialCatalog = databaseName
+        };
+        return csb.ConnectionString;
+    }
 
-            configuration.Bind(nameof(MigrationSettings), settings);
-            return settings;
-        }
+    private static string GetDatabaseName(string connectionString)
+    {
+        return new SqlConnectionStringBuilder(
+            connectionString).InitialCatalog;
+    }
+
+    private static FluentMigrator.Runner.IMigrationRunner CreateRunner(
+        string connectionString, MigrationSettings options)
+    {
+        var container = new ServiceCollection()
+            .AddFluentMigratorCore()
+            .ConfigureRunner(_ => _
+                .AddSqlServer()
+                .WithGlobalConnectionString(connectionString)
+                .ScanIn(typeof(MigrationRunner).Assembly).For.All())
+            .AddSingleton<MigrationSettings>(options)
+            .AddSingleton<ScriptResourceManager>()
+            .AddLogging(_ => _.AddFluentMigratorConsole())
+            .BuildServiceProvider();
+        return container.GetRequiredService<FluentMigrator.Runner.IMigrationRunner>();
+    }
+}
+
+// setting
+public class MigrationSettings
+{
+    public string ConnectionString { get; set; }
+    public static MigrationSettings Instance => new MigrationSettings();
+}
+
+public static class MigrationTools
+{
+
+    public static MigrationSettings GetConnectionString(this MigrationSettings settings)
+    {
+        var configuration =
+            new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("MigrationSetting.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+        configuration.Bind(nameof(MigrationSettings), settings);
+        return settings;
     }
 }
---
+++ Neximus.WorkShop/src/Neximus.WorkShop.Infrastructures/Neximus.WorkShop.Migrations/Migrations/_202505242120_AddOrderAndOrderItemTables.cs
@@ -0,0 +1,37 @@
+﻿using FluentMigrator;
+
+namespace Neximus.WorkShop.Migrations.Migrations;
+
+[Migration(202505242120)]
+public class _202505242120_AddOrderAndOrderItemTables : Migration
+{
+    public override void Down()
+    {
+        Delete.Table("OrderItems");
+        Delete.Table("Orders");
+    }
+
+    public override void Up()
+    {
+        Create.Table("Orders")
+            .WithColumn("Id").AsInt64().NotNullable().PrimaryKey().Identity()
+            .WithColumn("UserId").AsString(450).NotNullable()
+                 .ForeignKey("FK_Orders_Users", "Users", "Id")
+            .WithColumn("CreationDate").AsDateTime().NotNullable()
+            .WithColumn("PaymentDate").AsDateTime().NotNullable()
+            .WithColumn("TransactionNumber").AsString().NotNullable()
+            .WithColumn("TotalPrice").AsDecimal().NotNullable()
+            .WithColumn("TotalDiscount").AsDecimal().NotNullable();
+
+        Create.Table("OrderItems")
+              .WithColumn("Id").AsInt64().NotNullable().PrimaryKey().Identity()
+              .WithColumn("OrderId").AsInt64().NotNullable()
+                   .ForeignKey("FK_OrderItems_Orders", "Orders", "Id")
+                   .OnDelete(System.Data.Rule.Cascade)
+              .WithColumn("ProductId").AsInt64().NotNullable()
+              .WithColumn("Quantity").AsInt32().NotNullable()
+              .WithColumn("PricePerProduct").AsDecimal().NotNullable()
+              .WithColumn("TotalPrice").AsDecimal().NotNullable()
+              .WithColumn("TotalDiscount").AsDecimal().NotNullable();
+    }
+}
